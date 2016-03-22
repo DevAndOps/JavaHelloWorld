f93bfe323584c08da6642df299fbb6dcc6af466c
DevAndOps <shan612@gmail.com>
2016-03-22 16:20:05 +1100

    3rd commit is added

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 	// write your code here
         System.out.println("Hello World Java");
 	System.out.println("Hello Java, 2nd commit");
+	System.out.println("Hello Java, 3rd commit");
     }
 }
 
