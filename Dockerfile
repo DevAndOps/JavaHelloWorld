5fe6577ab33e2b8ac257043cb793a5315f36b24a
root <root@precise64.(none)>
2016-03-28 06:10:42 +0000

    Applied new working dir in docker file and java app can read that location

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,12 @@
 FROM java:7
 ENV ENV_VAL1=SHAN \
-    ENV_VAL2=CHOW
+    ENV_VAL2=CHOW \
+    WORKING_DIR="/ShanWorkSpace"
+RUN mkdir -p "$WORKING_DIR"
+WORKDIR $WORKING_DIR
+
 MAINTAINER Shan
+
 COPY HelloWorld.java .
 RUN ["javac", "HelloWorld.java"]
 
--- HelloWorld.java
+++ HelloWorld.java
@@ -9,6 +9,7 @@ public class HelloWorld {
 	System.out.println("Hello Java, 3rd commit");
 	System.out.println("Hello java 4th commit. This triggers auto-image build");
 	System.out.println("Env val1 is = " + System.getenv("ENV_VAL1"));
+	System.out.println("Current Directory is " + System.getProperty("user.dir"));
     }
 }
 
