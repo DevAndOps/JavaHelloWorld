36adc7538a92658e93e9614589b79e9ebda47c4a
root <root@precise64.(none)>
2016-03-28 04:58:29 +0000

    Reading from OS environment variables

--- HelloWorld.java
+++ HelloWorld.java
@@ -8,6 +8,7 @@ public class HelloWorld {
 	System.out.println("Hello Java, 2nd commit");
 	System.out.println("Hello Java, 3rd commit");
 	System.out.println("Hello java 4th commit. This triggers auto-image build");
+	System.out.println("Env val1 is = " + System.getenv("ENV_VAL1"));
     }
 }
 
