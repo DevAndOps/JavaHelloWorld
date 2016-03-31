c7e10811184b2beb4b6b247d7e2915629e027726
root <root@precise64.(none)>
2016-03-31 06:55:04 +0000

    Java app shows all passed arguments

--- HelloWorld.java
+++ HelloWorld.java
@@ -11,16 +11,9 @@ public class HelloWorld {
 	System.out.println("Env val1 is = " + System.getenv("ENV_VAL1"));
 	System.out.println("Current Directory is " + System.getProperty("user.dir"));
 	
-	String firstArg;
-	if (args.length > 0) {
- 	   try {
-        	firstArg = args[0];
-		System.out.println("Passed argument is " + firstArg);
- 	   } catch (NumberFormatException e) {
-        	System.err.println("Argument" + args[0] + " must be an integer.");
-        	System.exit(1);
-    		}
-	}
+	for (String arg: args) {
+            System.out.println("Passed argument " + arg);
+        }	
 
 	// coding ends
     }
