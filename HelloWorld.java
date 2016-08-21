5d23f778ba03a8490560f4b5cd04cc2813731074
shan.chowdhury <shan612@gmail.com>
2016-08-21 07:13:49 +0000

    ending comment with a period

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-	// write your code here
+	// write your code here.
         System.out.println("Hello World Java");
 	System.out.println("Hello Java, 2nd commit");
 	System.out.println("Hello Java, 3rd commit");
